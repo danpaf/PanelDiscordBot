3384eb35199635808362d637ed1825c4f7f694ff
danpaf <d4npaf@yandex.ru>
2023-01-19 13:13:04 +0300

    rdy bot

--- MainBot/Functions.cs
+++ MainBot/Functions.cs
@@ -1,6 +1,19 @@
+using DSharpPlus;
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
 namespace MainBot;
 
-public class Functions
+public class Funcs : BackgroundService
 {
-    
+    public async Task DeleteMessagesAsync(DiscordClient client, DiscordChannel channel, int count)
+    {
+        var messages = await channel.GetMessagesAsync(count);
+        await channel.DeleteMessagesAsync(messages);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.WriteLine("Funcs loaded");
+    }
 }
